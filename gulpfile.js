16489b30011d612f16d7f5d2c75ccc81bf8d5cfc
Yuriy-27 <yuriyshchebetun@gmail.com>
2019-08-12 18:36:49 +0300

    change menu button

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ function mySCSS () {
     return gulp.src('./*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./css'));
-  };
+  }
    
 gulp.task('watch', function () {
 gulp.watch('./*.scss', mySCSS);
